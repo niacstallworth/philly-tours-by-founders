ca11d3a2c89cf02209652d50bc83139485ac9d60
Nia stallworth <niacstallworth@gmail.com>
2026-01-25 08:53:47 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,10 +1,8 @@
 import AdminImport from './pages/AdminImport';
 import AdminMerchandise from './pages/AdminMerchandise';
-import AdminScavengerHunts from './pages/AdminScavengerHunts';
 import AdminTours from './pages/AdminTours';
 import Home from './pages/Home';
 import Merchandise from './pages/Merchandise';
-import ScavengerHunt from './pages/ScavengerHunt';
 import TourDetail from './pages/TourDetail';
 import __Layout from './Layout.jsx';
 
@@ -12,11 +10,9 @@ import __Layout from './Layout.jsx';
 export const PAGES = {
     "AdminImport": AdminImport,
     "AdminMerchandise": AdminMerchandise,
-    "AdminScavengerHunts": AdminScavengerHunts,
     "AdminTours": AdminTours,
     "Home": Home,
     "Merchandise": Merchandise,
-    "ScavengerHunt": ScavengerHunt,
     "TourDetail": TourDetail,
 }
 
